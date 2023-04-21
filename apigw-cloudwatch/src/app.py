5fcab5d2c9a72f40dfb39ef208f352fae97b5ac7
Elvis Ngwesse <elvisngwesseaws@gmail.com>
2023-04-21 04:17:59 +0100

    updates

--- apigw-cloudwatch/src/app.py
+++ apigw-cloudwatch/src/app.py
@@ -1,8 +1,21 @@
 import json
+import random
+import names
+
+
 def lambda_handler(event, context):
+    results = random.randint(1, 78)
+    pc1 = random.randint(1, 9)
+    pc2 = random.randint(10, 99)
+    street_name = names.get_last_name()
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Request received by Lambda function",
+            "Address":
+                {
+                    "House Number": f"{results}",
+                    "Street": f"{street_name} Street",
+                    "Post-Code": f"CV{pc1} {pc2}BT"
+                },
         }),
     }
